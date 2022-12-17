8ba6127b09375c0c24e9cd4c96a8d39101e27fa0
Pawel Nackowski <pawelnackowski@gmail.com>
2022-12-17 15:51:51 +0100

    add container and form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Container from "./Container";
+import Form from "./Form";
+
 function App() {
   return (
- <div>testowy tekst</div>
+    <Container>
+  <Form />
+    </Container>
   );
 }
 
