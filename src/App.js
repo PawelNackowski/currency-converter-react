0eb31f1f3fd2791267ed22a425e34bec4e9d66c5
Pawel Nackowski <pawelnackowski@gmail.com>
2022-12-18 06:28:50 +0100

    add result

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "./Container";
 import Section from "./Section";
 import Select from "./Select";
 import AmountInput from "./AmountInput";
+import Result from "./Result"
 
 function App() {
   return (
@@ -10,17 +11,9 @@ function App() {
       <form className="form">
         <fieldset className="form__fieldset">
           <Legend title="Kalkulator walut" />
-          <Section
-          title="Waluta:"
-          body={<Select />} 
-          />
-          <Section
-            title="Kwota:"
-            body={<AmountInput />}
-          />
-          <p>
-            <span className="form__valuePosition">Kwota w PLN : <strong>0.00</strong></span>
-          </p>
+          <Section title="Waluta:" body={<Select />} />
+          <Section title="Kwota:" body={<AmountInput />} />
+          <Result />
           <p>
             <input className="form__fieldInformation" name="sume" value="0.00" readonly />*
           </p>
---
+++ src/Result/index.js
@@ -0,0 +1,7 @@
+const Result = () => (
+    <p>
+      <span className="form__valuePosition">Kwota w PLN : <strong>0.00</strong></span>
+    </p>
+);
+
+export default Result;
