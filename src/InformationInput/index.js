78879780530100de115517d97c52419fc0fdc6c1
Pawel Nackowski <pawelnackowski@gmail.com>
2022-12-18 07:03:05 +0100

    change readonly to readOnly in informationInput

--- src/InformationInput/index.js
+++ src/InformationInput/index.js
@@ -1,6 +1,6 @@
 const InformationInput = () => (
   <p>
-    <input className="form__fieldInformation" name="sume" value="0.00" readonly />*
+    <input className="form__fieldInformation" name="sume" value="0.00" readOnly />*
   </p>
 );
 
