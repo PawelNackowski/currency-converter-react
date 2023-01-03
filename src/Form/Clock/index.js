f655507d1fc565ea28888abac4a7ba6094147c27
Pawel Nackowski <pawelnackowski@gmail.com>
2023-01-03 19:21:39 +0100

    added clock module

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,25 +1,32 @@
-import useCurrentDate from "./useCurrentData";
+import { useState, useEffect } from "react";
 import "./style.css";
 
 const Clock = () => {
-  const currentDate = useCurrentDate();
+  const [date, setDate] = useState(new Date());
 
-  const formattedDate = currentDate.toLocaleDateString
-  (undefined,
-    {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric"
-    }
-  );
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
-    <p className="clock">
-      Dziś jest: {formattedDate}
-    </p>
+    <div className="clock">
+      Dziś jest
+      {" "}
+      {date.toLocaleDateString(undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+      })}
+    </div>
   )
 };
 
