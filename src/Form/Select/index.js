ce764c5362860faa473e5f4b1e53dc7425d7f618
Pawel Nackowski <pawelnackowski@gmail.com>
2023-01-30 10:49:44 +0100

    added fetch with api to currencies

--- src/Form/Select/index.js
+++ src/Form/Select/index.js
@@ -1,18 +1,18 @@
 import { StyledSelect } from "./styled"
 
-const Select = ({ currencies, currency, setCurrency }) => (
+const Select = ({ ratesData, currency, setCurrency }) => (
   <StyledSelect
     value={currency}
     onChange={({ target }) => setCurrency(target.value)}
   >
-    {currencies.map(currency => (
+    {Object.keys(ratesData.rates).map(((currency) => (
       <option
-        key={currency.id}
-        value={currency.name}
+        key={currency}
+        value={currency}
       >
-        {currency.name}
+        {currency}
       </option>
-    ))}
+    )))}
   </StyledSelect>
 );
 
