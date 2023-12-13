733afe9b5c04a55d091484ffa0fe4d2e1f45ee30
Pawel Nackowski <pawelnackowski@gmail.com>
2023-12-13 15:31:30 +0100

    changed getRate function

--- src/Form/index.js
+++ src/Form/index.js
@@ -13,11 +13,10 @@ const Form = () => {
   const ratesData = useRatesData();
 
   const getRate = () => {
-    const rate = ratesData.rates[currency];
-
-      if (ratesData.status === "success") {
-          return 1/rate;
-          };
+    if (ratesData.status === "success" && ratesData.conversion_rates) {
+      const rate = ratesData.conversion_rates[currency];
+      return 1 / rate;
+    }
   };
 
   const onFormSubmit = (event) => {
@@ -37,6 +36,7 @@ const Form = () => {
             )
           : (
             ratesData.status === "error" ? (
+              console.log("błąd w pobieraniu"),
               <p>
                 Nastąpił błąd przy pobieraniu danych...<br/>
                 Sprawdź swoje połączenie z internetem, albo skontaktuj się z usługodawcą strony.
